75d3206ac7444903499c951666b49a43c0677f4c
김성수 <85716720+SsangSoo@users.noreply.github.com>
2023-03-06 12:45:57 +0900

    [Bronze V] Title: A-B, Time: 204 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class Main {
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+	        int input1 = scanner.nextInt();
+	        int A = input1;
+	        int input2 = scanner.nextInt();
+	        int B = input2;
+	       System.out.println(A-B);
+		}
+	}
