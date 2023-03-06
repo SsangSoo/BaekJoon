bcd21eeae2a32c0d90552f19f349021a77364139
김성수 <85716720+SsangSoo@users.noreply.github.com>
2023-03-06 12:46:29 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17964 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double a = s.nextDouble();
+        double b = s.nextDouble();
+        System.out.println(a/b);
+        
+        s.close();
+        
+    }
+}
