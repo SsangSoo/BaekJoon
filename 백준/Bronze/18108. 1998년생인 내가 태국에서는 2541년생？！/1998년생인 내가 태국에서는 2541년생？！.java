881add3a67f4ad69d590bf8bc45c70dde7e03658
김성수 <85716720+SsangSoo@users.noreply.github.com>
2024-10-20 06:23:39 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 164 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int immortalPrayer = sc.nextInt();
+        int immortalPrayerStartYear = -544;
+
+        System.out.println(immortalPrayer + immortalPrayerStartYear + 1);
+    }
+}
