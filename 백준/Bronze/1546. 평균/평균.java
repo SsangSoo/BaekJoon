5ab52b9cdf17aa593ed32795f5f344c35f87c51c
김성수 <85716720+SsangSoo@users.noreply.github.com>
2025-11-02 08:36:40 +0900

    [Bronze I] Title: 평균, Time: 108 ms, Memory: 14644 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,25 +1,43 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int count = sc.nextInt();
-        int totalScore = 0;
-        int max = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for (int i = 0; i < count; i++) {
-            int score = sc.nextInt();
-            totalScore += score;
+        // n을 입력받는다.
+        int n = Integer.parseInt(br.readLine());
+        // 점수를 담을 n개의 수를 담을 배열을 생성해놓는다.
+        int[] scoreArr = new int[n];
 
-            if(max < score) {
-                max = score;
-            }
+        // 문자열을 입력받는다.
+        String input = br.readLine();
+        // 배열처리 한다.
+        String[] scores = input.split(" ");
+        // 처리한 배열을 이전에 생성한 배열로 담는다.
+        for(int i = 0; i < scoreArr.length; i++) {
+            scoreArr[i] = Integer.parseInt(scores[i]);
         }
 
-        float average = ((float) totalScore / max * 100) / count;
+        // 최댓값을 구한다.
+            // Math 클래스 사용
+        int max = scoreArr[0];
+        for(int i = 1; i < scoreArr.length; i++) {
+            max = Math.max(max, scoreArr[i]);
+        }
 
-        System.out.println(average);
 
+        // 배열을 순회하면서 총합을 구한다.
+            // score / max * 100
+        double sum = 0;
+        for(int score : scoreArr) {
+            sum += (double) score / max * 100;
+        }
+
+        bw.write(String.format("%.2f", (sum / n)));
+
+        br.close();
+        bw.close();
     }
 }
