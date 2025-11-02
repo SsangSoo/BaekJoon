76833863a58ff01b712f6cde1198c51d6ae48604
김성수 <85716720+SsangSoo@users.noreply.github.com>
2025-11-02 14:51:59 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 844 ms, Memory: 124800 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -7,17 +7,23 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-       
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] sumArr = new int[N+1][N+1];
+        int[][] arr = new int[N+1][N+1];
 
         for(int i=1; i<=N; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=1; j<=N; j++) {
-                sumArr[i][j] = sumArr[i][j-1] + Integer.parseInt(st.nextToken());
+                arr[i][j] = arr[i][j] + Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] sumArr = new int[N+1][N+1];
+        for(int i=1; i<=N; i++) {
+            for(int j=1; j<=N; j++) {
+                sumArr[i][j] = sumArr[i - 1][j] + sumArr[i][j - 1] - sumArr[i - 1][j - 1] + arr[i][j];
             }
         }
 
@@ -28,10 +34,8 @@ public class Main {
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            int sum = 0;
-            for(int i=x1; i<=x2; i++) {
-                sum += sumArr[i][y2] - sumArr[i][y1-1];
-            }
+            int sum = sumArr[x2][y2] - sumArr[x1 - 1][y2] - sumArr[x2][y1 - 1] + sumArr[x1 - 1][y1 - 1];
+
             bw.write(sum + "\n");
         }
 
