1c3768521e5e5fdc8264b529d4a8fcfbfe99970d
김성수 <85716720+SsangSoo@users.noreply.github.com>
2025-11-05 08:05:25 +0900

    [Gold IV] Title: 좋다, Time: 164 ms, Memory: 15232 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[N];
+
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int result = 0;
+        for(int k=0; k < N; k++) {
+            int goodNumber = arr[k];
+            int i = 0;
+            int j = N-1;
+            while(i < j) {
+                if(i==k) {
+                    i++;
+                    continue;
+                } else if(j==k) {
+                    j--;
+                    continue;
+                }
+
+                int sum = arr[i] + arr[j];
+                if(sum <  goodNumber) {
+                    i++;
+                } else if(sum == goodNumber) {
+                    result++;
+                    break;
+                } else {
+                    j--;
+                }
+            }
+        }
+
+        bw.write(String.valueOf(result));
+
+        br.close();
+        bw.close();
+    }
+}
